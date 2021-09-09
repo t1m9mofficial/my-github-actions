1f0dfb39c6c3e2801627fed8382cfa2de50af28b
Telepathic Programming School <tps@Tahsins-MacBook-Pro.local>
2021-09-09 15:42:43 +0600

    Back to the Future

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function mooncakeSays(message) {
-  let mooncake = '(O.o)';
+  let mooncake = '(o.o)';
 
-  if ( !message ) mooncake;
+  if ( !message ) {
+    return `${mooncake} chookity?`;
+  }
 
   return `${mooncake} ${message}`;
 }
